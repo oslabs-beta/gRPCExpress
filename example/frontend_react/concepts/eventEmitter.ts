77c4921b70a521c68efe140d28e0238bdd18bbe1
Jun <junwenzhang@outlook.com>
2023-09-08 19:25:42 -0400

    Added adjust capacity function to the store

--- example/frontend_react/concepts/eventEmitter.ts
+++ example/frontend_react/concepts/eventEmitter.ts
@@ -1,14 +1,32 @@
 class EventEmitter {
-  #store: { [key: string]: any };
+  #store: {
+    data: Map<string, any>;
+    expire: any;
+    capacity: number; // ~5MB
+  };
 
   constructor() {
-    this.#store = this.loadStore() || {};
+    this.#store = this.loadStore() || this.initStore();
     this.syncStore = this.syncStore.bind(this);
     window.addEventListener('beforeunload', this.syncStore);
   }
 
   subscribe(key: string, options: { [key: string]: any }) {
-    this.#store[key] = options;
+    if (this.#store.data.size % 100 === 0) {
+      this.adjustCapacity();
+    }
+
+    if (
+      this.#store.data.size === this.#store.capacity &&
+      !this.#store.data.has(key)
+    ) {
+      const [[k]] = this.#store.data;
+      this.#store.data.delete(k);
+    } else if (this.#store.data.has(key)) {
+      this.#store.data.delete(key);
+    }
+
+    this.#store.data.set(key, options);
   }
 
   unsubscribe(key: string) {
@@ -16,10 +34,14 @@ class EventEmitter {
   }
 
   get(key: string) {
-    return this.#store[key];
+    const value = this.#store.data.get(key);
+    this.#store.data.delete(key);
+    this.#store.data.set(key, value);
+    return value;
   }
 
   syncStore() {
+    this.adjustCapacity();
     localStorage.setItem('grpcExpressStore', JSON.stringify(this.#store));
   }
 
@@ -27,6 +49,27 @@ class EventEmitter {
     const store = localStorage.getItem('grpcExpressStore');
     if (store) {
       return JSON.parse(store);
+    }
+  }
+
+  initStore(capacity: number = 100) {
+    const cache = new Map();
+    return {
+      data: cache,
+      expire: null,
+      capacity: capacity,
+    };
+  }
+
+  adjustCapacity() {
+    // calculate the size of the store
+    const size = JSON.stringify(this.#store.data).length; // >= 2500000
+    if (size > 2000000) {
+      // calculate the size per object
+      const perObjectSize = size / this.#store.data.size;
+      // estimate the max capacity
+      const maxCapacity = Math.floor(2500000 / perObjectSize);
+      this.#store.capacity = maxCapacity;
     }
   }
 }
