bd074839840c4d6856ea4756264d63dbca864589
Shi Yu Liu <shiyu0811liu@gmail.com>
2023-09-08 22:01:31 -0400

    added cache expiration

--- example/frontend_react/concepts/eventEmitter.ts
+++ example/frontend_react/concepts/eventEmitter.ts
@@ -4,12 +4,27 @@ class EventEmitter {
   constructor() {
     this.#store = {};
   }
+  
+  // method taking in key and session time
+  timer(key: string, sessionMaxTime: number) {
+    // give key an expire property that is the setTimeout to delete cache with key key
+    this.#store[key]['expire'] = setTimeout(() =>
+      { 
+          this.unsubscribe(key);
+      }, sessionMaxTime * 1000,
+      sessionMaxTime, key);
+  }
 
-  subscribe(key: string, options: { [key: string]: any }) {
+  subscribe(key: string, options: { [key: string]: any }, sessionMaxTime: number) {
     this.#store[key] = options;
+    // caching will create a timer
+    this.timer(key, sessionMaxTime);
   }
 
   unsubscribe(key: string) {
+    if (this.#store[key]['expire']) {
+      clearTimeout(this.#store[key]['expire']);
+    }
     delete this.#store[key];
   }
 
